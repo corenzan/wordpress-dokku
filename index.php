a1c082fe1e65966b2e275b1b18ffc63e9d775bed
Arthur Corenzan <arthur@corenzan.com>
2017-10-06 17:11:11 -0300

    Move all configuration to wp-config.php

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
 <?php
-// Yea use themes.
-define( 'WP_USE_THEMES', true );
-
-// Update wp-content path.
-// https://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content_folder
-define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
-define( 'WP_CONTENT_URL', '//' . $_SERVER['HTTP_HOST'] . '/wp-content' );
-
-// Load from Wordpress installation.
-require( dirname( __FILE__ ) . '/wordpress/wp-blog-header.php' );
+require( dirname( __FILE__ ) . '/wordpress/index.php' );
--- wp-config.php
+++ wp-config.php
@@ -81,6 +81,11 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', (bool)getenv('WP_DEBUG'));
 
+// Update wp-content path.
+// https://codex.wordpress.org/Editing_wp-config.php#Moving_wp-content_folder
+define( 'WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content' );
+define( 'WP_CONTENT_URL', '//' . $_SERVER['HTTP_HOST'] . '/wp-content' );
+
 // If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
 // see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
